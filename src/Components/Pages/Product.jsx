7e53efa9168b673333ab9dbb6ac4aa9d92647be4
Adhishthan <adhishthanashok@gmail.com>
2024-07-06 19:12:30 +0530

    Added more styling to add to cart button

--- src/Components/Pages/Product.jsx
+++ src/Components/Pages/Product.jsx
@@ -40,7 +40,7 @@ const Product = () => {
               <a
                 href="#"
                 title=""
-                className="text-gray-800 dark:text-gray-50 mt-4 sm:mt-0 bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 flex items-center justify-center"
+                className="text-gray-50 dark:text-gray-50 mt-4 sm:mt-0 bg-green-500 hover:bg-primary-800 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800 flex items-center justify-center"
                 role="button"
               >
                 <svg
